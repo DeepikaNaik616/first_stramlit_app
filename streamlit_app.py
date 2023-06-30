0ccec69570ff528353d79328f56a73b70cdff49f
DeepikaNaik616 <114483358+DeepikaNaik616@users.noreply.github.com>
2023-06-30 16:38:35 +0530

    Update streamlit_app.py
    with proper icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('🥣My Parents new Healthy Dinner')
-streamlit.header('🥗Breakfast Menu')
-streamlit.text('🐔Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑Kale, Spinach & Rocket Smootie')
-streamlit.text('🍞Hard-Boiled Free-Range Egg')
+streamlit.title('My Parents new Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smootie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
